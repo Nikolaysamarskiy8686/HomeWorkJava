2b35f01d2b51bf3f3b40e20afb83cc9b1d82706c
Nikolay Samarskiy <kolya.samarskiy.2015@mail.ru>
2023-01-31 22:40:37 +0500

    ewrqwer

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int summa = 1200;
+        int mill = 20;
+        int x = summa/mill;
+        System.out.println(x);
+    }
+}
